0b32a88d049220826a4051853a5d0bde85eeee14
Nadia Lee <miss.nadia.lee@gmail.com>
2018-04-06 22:55:23 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,8 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/reversename/:name' do
-    @name = params[:name]
-    @name.reverse
+    params[:name].reverse
   end
 
   get '/square/:number' do
