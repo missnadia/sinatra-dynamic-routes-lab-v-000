51519e37cfbab51a6ed71d35bf09e1cce40163fa
Nadia Lee <miss.nadia.lee@gmail.com>
2018-04-06 22:52:54 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,36 +7,36 @@ class App < Sinatra::Base
   end
 
   get '/reversename/:name' do
-    @name = param[:name]
+    @name = params[:name]
     @name.reverse
   end
 
   get '/square/:number' do
-    @number = param[:number]
+    @number = params[:number]
     @number.to_i^2
     "#{@number}"
   end
 
   get '/say/:number/:phrase' do
-    @number = param[:number]
-    @phrase = param[:phrase]
+    @number = params[:number]
+    @phrase = params[:phrase]
     @repeat = @phrase * @number
     "#{@repeat}"
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @word1 = param[:word1]
-    @word2 = param[:word2]
-    @word3 = param[:word3]
-    @word4 = param[:word4]
-    @word5 = param[:word5]
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
     "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}"
   end
 
   get '/:operation/:number1/:number2' do
-    @operation = param[:operation]
-    @number1 = param[:number1]
-    @number2 = param[:number2]
+    @operation = params[:operation]
+    @number1 = params[:number1]
+    @number2 = params[:number2]
     if @operation == add
       @number1 + @number2
     elsif @operation == subtract
