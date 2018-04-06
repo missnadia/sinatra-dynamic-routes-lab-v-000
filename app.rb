a3a3771bc6e56461e775eac0887ff82a405a3ff5
Nadia Lee <miss.nadia.lee@gmail.com>
2018-04-06 22:53:52 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -9,8 +9,7 @@ class App < Sinatra::Base
 
   get '/square/:number' do
     @number = params[:number]
-    @number.to_i^2
-    "#{@number}"
+    @number.to_i^2.to_s
   end
 
   get '/say/:number/:phrase' do
